7a3db066c03bf4adc3ce2bb090f95274bb3e0a63
Vadim <v_a_d@inbox.ru>
2020-08-26 17:49:42 +0300

    Updated application

--- src/application.jsx
+++ src/application.jsx
@@ -1,26 +1,62 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit'
-import Layout from './components/Layout';
-import { channelsReduser, messagesReduser, currentChannelIdReduser } from './reducers';
-import { receivingMessage } from './actions';
-import UserNameContext from './context'; 
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import Layout from './Layout.jsx';
+import UserNameContext from './context';
+import './locales';
+import {
+  messagesSlice,
+  currentChannelIdSlice,
+  channelsSlice,
+  modalStateSlice,
+} from './features';
 
 export default (initState, socket, userName) => {
   const store = configureStore({
     reducer: {
-      channels: channelsReduser,
-      messages: messagesReduser,
-      currentChannelId: currentChannelIdReduser,
+      channels: channelsSlice.reducer,
+      messages: messagesSlice.reducer,
+      currentChannelId: currentChannelIdSlice.reducer,
+      modalState: modalStateSlice.reducer,
     },
+    middleware: getDefaultMiddleware({
+      serializableCheck: false,
+    }),
     devTools: process.env.NODE_ENV !== 'production',
     preloadedState: initState,
   });
 
-  socket.on('newMessage', function (data) {
+  const { receivingMessage } = messagesSlice.actions;
+
+  socket.on('newMessage', (data) => {
     const { data: { attributes } } = data;
     store.dispatch(receivingMessage(attributes));
+  });
+
+  const {
+    receivingNewChannel,
+    receivingRemoveChannel,
+    receivingRenameChannel,
+  } = channelsSlice.actions;
+
+  const { changeCurrentChannel } = currentChannelIdSlice.actions;
+
+  socket.on('newChannel', (data) => {
+    const { data: { id, attributes } } = data;
+    store.dispatch(receivingNewChannel(attributes));
+    store.dispatch(changeCurrentChannel(id));
+  });
+
+  socket.on('removeChannel', (data) => {
+    const { data: { id } } = data;
+    store.dispatch(receivingRemoveChannel(id));
+    store.dispatch(changeCurrentChannel(1));
+  });
+
+  socket.on('renameChannel', (data) => {
+    const { data: { id, attributes } } = data;
+    store.dispatch(receivingRenameChannel({ id, attributes }));
   });
 
   render(
