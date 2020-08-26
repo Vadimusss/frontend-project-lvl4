d83a99f76444fe4173103e2783c970c201e16ddc
Vadim <v_a_d@inbox.ru>
2020-08-26 21:28:48 +0300

    Added facade

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Channels from './Сhannels.jsx';
+import Messages from './Messages.jsx';
+import MessagesForm from './MessagesForm.jsx';
+import Notification from './Notification.jsx';
+
+export {
+  Channels,
+  Messages,
+  MessagesForm,
+  Notification,
+};
